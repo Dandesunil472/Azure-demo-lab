a3cedfadc0518d9f096ed9999d1ebe2e9ed0998a
Dandesunil472 <reddysunil472@gmail.com>
2025-10-29 22:04:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create Resource Group with tags
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+
+  tags = {
+    App        = "100"
+    TechOwner  = "Sunil"
+  }
+}
